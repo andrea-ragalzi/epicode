557727d78ab390d1abdd34aeddaa13c8cf456e1a
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-02-06 17:13:08 +0100

    add second js with var

---
+++ week_2/day_1/esercizi/assets/js/naming.js
@@ -0,0 +1,11 @@
+var var_pantera = 'pantera';
+var varCriceto = 'criceto';
+var VarGiraffa = 'giraffa';
+var VAR_LEONE = 'leone';
+
+console.log(varCriceto);
+console.log(VarGiraffa);
+console.log(VAR_LEONE);
+
+document.write(var_pantera + ' scritto da Javascript');
+document.getElementById('stringa1').innerHTML = 'output scritto da Javascript';
