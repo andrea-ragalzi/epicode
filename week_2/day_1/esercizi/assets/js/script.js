05b06663603b0c808848f7a5defe3a644f08c8df
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-02-06 17:13:30 +0100

    removed var

--- week_2/day_1/esercizi/assets/js/script.js
+++ week_2/day_1/esercizi/assets/js/script.js
@@ -1,14 +1,2 @@
 alert('Usa il pop up per dare dei messaggi');
 alert('Sto studiando JS');
-
-var var_pantera = 'pantera';
-var varCriceto = 'criceto';
-var VarGiraffa = 'giraffa';
-var VAR_LEONE = 'leone';
-
-console.log(varCriceto);
-console.log(VarGiraffa);
-console.log(VAR_LEONE);
-
-document.write(var_pantera + ' scritto da Javascript');
-document.getElementById('stringa1').innerHTML = 'output scritto da Javascript';
