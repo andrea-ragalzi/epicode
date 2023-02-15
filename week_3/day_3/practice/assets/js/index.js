49f54eafdfd058f6e500deef9cdd91d85394d603
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-02-15 17:22:15 +0100

    add completed task

--- week_3/day_3/practice/assets/js/index.js
+++ week_3/day_3/practice/assets/js/index.js
@@ -1,6 +1,36 @@
 const addTaskButton = document.getElementById("addTask");
 const taskList = document.getElementById("taskList");
 
+var createDeleteButton = (taskItem) => {
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("Button");
+    deleteButton.textContent = "Elimina";
+    taskItem.appendChild(deleteButton);
+    return deleteButton;
+}
+
+var createLabelItem = (taskItem) => {
+    let labelItem = document.createElement("label");
+    labelItem.textContent = newTask.value;
+    labelItem.classList.add("custom-label");
+    labelItem.addEventListener("click", function (event) {
+        if (labelItem.style.textDecoration === "line-through") {
+            labelItem.style.textDecoration = 'none';
+        }
+        else {
+            labelItem.style.textDecoration = "line-through";
+        }
+    });
+    taskItem.appendChild(labelItem);
+}
+
+var createTaskItem = () => {
+    let taskItem = document.createElement("li");
+    taskItem.classList.add("custom-li");
+    taskList.appendChild(taskItem);
+    return taskItem;
+}
+
 addTaskButton.addEventListener("click", function (event) {
     const newTask = document.getElementById("newTask");
     if (!newTask.value) {
@@ -8,19 +38,11 @@ addTaskButton.addEventListener("click", function (event) {
         return;
     }
 
-    const taskItem = document.createElement("li");
-    taskItem.classList.add("custom-li");
-    taskList.appendChild(taskItem);
+    const taskItem = createTaskItem();
 
-    const labelItem = document.createElement("label");
-    labelItem.textContent = newTask.value;
-    labelItem.classList.add("custom-label");
-    taskItem.appendChild(labelItem);
+    createLabelItem(taskItem)
 
-    const deleteButton = document.createElement("button");
-    deleteButton.classList.add("delete");
-    deleteButton.textContent = "Elimina";
-    taskItem.appendChild(deleteButton);
+    const deleteButton = createDeleteButton(taskItem);
 
     deleteButton.addEventListener("click", function () {
         taskList.removeChild(taskItem);
