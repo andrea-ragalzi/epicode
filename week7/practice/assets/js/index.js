5970343cb51f75a25c731429976ec42ed789db62
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-03-13 16:58:40 +0100

    ex w7d1

---
+++ week7/practice/assets/js/index.js
@@ -0,0 +1,72 @@
+class User {
+    constructor(_firstName, _lastName, _age, _location) {
+        this.firstName = _firstName;
+        this.lastName = _lastName;
+        this.age = _age;
+        this.location = _location;
+    }
+    isOlder(other_user) {
+        if (this.age > other_user.age) {
+            console.log(`${this.firstName} è più vecchio di ${other_user.firstName}`);
+            return 1;
+        }
+        else if (this.age < other_user.age) {
+            console.log(`${this.firstName} è più giovane di ${other_user.firstName}`);
+            return -1;
+        }
+        else {
+            console.log(`${this.firstName} e ${other_user.firstName} hanno la stessa età`);
+            return 0;
+        }
+    }
+}
+
+class Pet {
+    constructor(_petName, _ownerName, _species, _breed) {
+        this.petName = _petName;
+        this.ownerName = _ownerName;
+        this.species = _species;
+        this.breed = _breed;
+    }
+    hasSameOwner(other_pet) {
+        return this.ownerName === other_pet.ownerName;
+    }
+}
+
+const pushPetOnList = (newPet) => {
+    const petRef = document.createElement('li');
+    petRef.textContent =
+        `${newPet.petName}, 
+        ${newPet.ownerName},
+        ${newPet.species},
+        ${newPet.breed}`;
+    petListRef.appendChild(petRef);
+    formRef.reset();
+}
+
+const formRef = document.getElementById('formAnimal');
+const submitButtonRef = document.getElementById('submitButton');
+const petListRef = document.getElementById('petList');
+
+mario = new User('Mario', 'Biondi', 50, 'Busalla');
+luigi = new User('Luigi', 'Ottavi', 60, 'Sezzadio');
+
+console.log(mario);
+
+console.log(mario.isOlder(luigi));
+console.log(luigi.isOlder(mario));
+luigi.age = mario.age;
+console.log(mario.isOlder(luigi));
+
+submitButtonRef.addEventListener('click', (event) => {
+    event.preventDefault();
+    const petName = document.getElementById('petName').value;
+    const ownerName = document.getElementById('ownerName').value;
+    const species = document.getElementById('species').value;
+    const breed = document.getElementById('breed').value;
+    const newPet = new Pet(petName, ownerName, species, breed);
+    pushPetOnList(newPet);
+});
+
+
+
